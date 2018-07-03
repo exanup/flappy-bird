4e37217de0b248e869fce9cc5306e68f1149316d
Anup Dhakal <anupdhakal2000@gmail.com>
2018-07-04 00:19:18 +0545

    Testing sprite loading and sprite animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,24 @@
-import EntityHitBox from "./modules/EntityHitBox.js";
+import Bird from "./modules/Bird.js";
 
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d", {
   alpha: false // don't forget to make it false for better performance
 });
 
-let box = {
+let bird = new Bird({
   x: 10,
   y: 10,
-  height: 200,
-  width: 200,
-};
-
-box.hitBox = new EntityHitBox({
-  parent: box,
+  width: 34,
+  height: 24,
 });
 
-box.hitBox.show(ctx);
+let raf = undefined;
+const animate = function() {
+  raf = requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  bird.draw(ctx);
+  bird.update();
+};
+animate();
+
+// console.log(bird);
